78cd94a98bfc9cd397ca51e743b4c07a62a02ba7
celine <atakikoo@gmail.com>
2019-09-17 17:28:39 +0200

    Add app

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './home/home'
+import Header from './header/header'
+import Footer from './footer/footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
