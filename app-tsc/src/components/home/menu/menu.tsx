189d127c09aeacb255c7c2792fa5bd272a48927e
celine <atakikoo@gmail.com>
2019-09-23 12:38:22 +0200

    add title props

--- app-tsc/src/components/home/menu/menu.tsx
+++ app-tsc/src/components/home/menu/menu.tsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import './menu.scss'
+import SubMenu from './sub-menu/sub-menu'
 
 const Menu = () => {
         return (
             <div className="menu">
-                <div className="menu-item right blue">
-                    Livres
+                <div className="menu-item right blue-bc">
+                    <SubMenu title="Livres"></SubMenu>
                 </div>
-                <div className="menu-item left yellow">
-                    Kimono
+                <div className="menu-item left yellow-bc">
+                    <SubMenu title="Kimono"></SubMenu>
                 </div>
-                <div className="menu-item right red">
-                    Japan
+                <div className="menu-item right red-bc">
+                    <SubMenu title="Japon"></SubMenu>
                 </div>
-                <div className="menu-item left violet">
-                    Job
+                <div className="menu-item left violet-bc">
+                <SubMenu title="Job"></SubMenu>
                 </div>
             </div>
         );
