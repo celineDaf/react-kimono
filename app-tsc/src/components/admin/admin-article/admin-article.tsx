8b7c98ac89ad763ac023f8753c057fad269da5f7
celine <atakikoo@gmail.com>
2019-10-02 18:23:09 +0200

    add tags management on admin-article

--- app-tsc/src/components/admin/admin-article/admin-article.tsx
+++ app-tsc/src/components/admin/admin-article/admin-article.tsx
@@ -1,10 +1,83 @@
-import React from 'react';
-
-interface Props {
+import React, { useState, SetStateAction } from "react";
+import { CategoriesList, CategoriesEnum, Categories } from "../admin-categories/category-types";
+import "./admin-article.scss"
+interface PropsType {
+  
 }
 
-const AdminArticle = (props: Props) => {
-    return null;
+const AdminArticle = (props: PropsType) => {
+  const [tag, setTag] = useState<string>("");
+  const [tags, setTags] = useState<string[]>([]);
+
+  const categories: Categories[] = CategoriesList;
+  let category: string = CategoriesEnum.BOOKS;
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement> ) => {
+    category = event.currentTarget.value;
+  };
+  
+    const handleChange = (event: React.ChangeEvent<HTMLFormElement> ): void => {
+      
+    };
+  
+    const handleAddTag = (): void => {
+      if (!tag || tag.length == 0 ) {return;}
+      setTags([...tags, tag]);
+      setTag('');
+    };
+    const handleRemoveTag = (tagToRemove:string): void => {
+      setTags(tags.filter((t) => t !== tagToRemove));
+    };
+
+
+  
+  return (
+    <div className="admin-article">
+      <form>
+        <div className="section">
+          <label>
+            Nom du nouvel article :
+            <input type="text" name="name" />
+          </label>
+        </div>
+
+        <div>
+          <select value={category} onChange={handleSelectChange}>
+            {categories.map(i => {
+              return (
+                <option value="{i}" key={i}>
+                  {i}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <div>
+          <label>
+            Tags
+            <div>
+            { tags && tags.length > 0 && (
+            tags.map( (i: string) => {
+              return (
+                <div className="selected-tag">{i} 
+                <div className="tag-to-remove" onClick={() => handleRemoveTag(i)}>x</div>
+                </div>
+              );
+            })
+            )}
+            </div>
+            <input type="text" name="tag" value={tag} onChange={event => setTag(event.target.value) }/>
+            <button type="button" onClick={() => handleAddTag()}>Ajouter</button>
+          </label>
+        </div>
+
+        <div>
+          <button type="button" className="button">Créer</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AdminArticle;
