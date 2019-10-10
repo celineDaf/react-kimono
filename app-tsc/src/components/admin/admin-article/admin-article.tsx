01dd9dec9c1012ab0673e27fed9a944c1d05ed50
celine <atakikoo@gmail.com>
2019-10-10 12:51:32 +0200

    manage article update (except photos) and binding

--- app-tsc/src/components/admin/admin-article/admin-article.tsx
+++ app-tsc/src/components/admin/admin-article/admin-article.tsx
@@ -1,37 +1,86 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./admin-article.scss";
 import CategorySelector from "../../../shared/category-selector/category-selector";
 import TagsManagement from "../tags-management/tags-management";
+import { IArticle, ArticleInit } from "../../../domain/article-type";
+import { Category } from "../../../domain/category-types";
+import AdminTextManagement from "../admin-text-management/admin-text-management";
 
-interface PropsType {}
+interface Props {
+  onChangeArticle: (article: IArticle) => void;
+}
 
-const AdminArticle = (props: PropsType) => {
-  const handleChange = (event: React.ChangeEvent<HTMLFormElement>): void => {};
+const AdminArticle = (props: Props) => {
+  const [article, setArticle] = useState<IArticle>(ArticleInit);
+
+  const onChangeCategory = (selectedCategory: Category) => {
+    setArticle({ ...article, category: selectedCategory });
+  };
+  const onChangeTags = (updatedTags: string[]) => {
+    setArticle({ ...article, tags: updatedTags });
+  };
+  const onChangeText = (updatedText: string) => {
+    setArticle({ ...article, text: updatedText });
+  };
+  const onChangeTitle = (updatedTitle: string) => {
+    setArticle({ ...article, title: updatedTitle });
+  };
+
+  useEffect(() => {
+    props.onChangeArticle(article);
+  }, [article]);
+
+  const handleCreation = () => {
+    console.log(article);
+  };
 
   return (
     <div className="admin-article container">
-      <form className="row">
+      <form className="row violet-bc">
         <div className="column">
           <div className="section">
-            <label>
-              <input type="text" name="name" placeholder="Nom de l'article" />
-            </label>
-          </div>
-          <CategorySelector />
-          <TagsManagement />
+            <div className="sub-section">
+              <span className="input-gradient">
+                <input
+                  type="text"
+                  name="title"
+                  className=""
+                  placeholder="Nom de l'article"
+                  onChange={event => onChangeTitle(event.target.value)}
+                />
+              </span>
+            </div>
 
-          <div>Ajouter des photos</div>
+            <div className="sub-section">
+              <CategorySelector onChangeCategory={onChangeCategory} />
+            </div>
+
+            <div className="sub-section">
+              <TagsManagement onChangeTags={onChangeTags} />
+            </div>
+
+            <div className="sub-section">
+              <div className="sub-title"> - Ajouter des photos</div>
+            </div>
+          </div>
         </div>
 
         <div className="column">
-          <textarea
-            name="text"
-            rows={20}
-            className="article-content-input"
-          ></textarea>
-          <button type="button" className="button">
-            Créer
-          </button>
+          <section>
+            <div className="sub-section">
+            <AdminTextManagement onChangeText={onChangeText} />
+            </div>
+
+            <div className="sub-section">
+            <button
+              type="button"
+              className="button"
+              onClick={() => handleCreation()}
+            >
+              Créer
+            </button>
+            </div>
+          </section>
         </div>
       </form>
     </div>
