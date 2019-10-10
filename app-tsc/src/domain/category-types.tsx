ae37ec88596e6fdbb72e6d21809759c6267cf30c
celine <atakikoo@gmail.com>
2019-10-10 12:48:16 +0200

    update iterface IArticle and add ArticleInit

--- app-tsc/src/domain/category-types.tsx
+++ app-tsc/src/domain/category-types.tsx
@@ -3,9 +3,9 @@ export enum CategoriesEnum  {
     JAPAN = 'Japan', JOB ='Job', KIMONO = 'kimono', BOOKS = 'books'
 };
 
-export type Categories = CategoriesEnum.BOOKS | CategoriesEnum.JAPAN |CategoriesEnum.JOB | CategoriesEnum.KIMONO;
+export type Category = CategoriesEnum.BOOKS | CategoriesEnum.JAPAN |CategoriesEnum.JOB | CategoriesEnum.KIMONO;
 
-export const CategoriesList: Categories[] = [ 
+export const CategoriesList: Category[] = [ 
     CategoriesEnum.BOOKS,
     CategoriesEnum.JAPAN,
     CategoriesEnum.JOB,
