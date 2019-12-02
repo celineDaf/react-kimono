c7c88500eb5fd72af6a975b997d15ad9bf959222
celine <atakikoo@gmail.com>
2019-12-02 18:13:34 +0100

    fetch  articles in admin

--- app-tsc/src/components/admin/admin.tsx
+++ app-tsc/src/components/admin/admin.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./admin.scss"
 import { Link } from 'react-router-dom';
-import { IArticle } from '../../domain/article-type'
 import ArticlesList from '../app-container/articles/article-list';
+import firebase from '../../firebase'
+import CategorySelector from '../../shared/category-selector/category-selector'
 
 const Admin = () => {
 
-    let articles: IArticle[] = [];
+    const [articles, setArticles]  = useState();
+    
+    useEffect(() => {
+        firebase.firestore().collection('articles')
+        .onSnapshot((snapshot)=> {
+            const articles = snapshot.docs.map((doc)=> ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            console.log('admin articles', articles)
+            setArticles(articles);
+        })
+    })
+    
+    const onCategoryChoice = (category: string) => {
+        if (category === undefined) {
+            setArticles(articles)
+        } else {
+            const filteredArticles = articles.filter((art) => art.category === category);
+            setArticles(filteredArticles)
+            console.log('filteredArticles', filteredArticles)
+        }
+       
+    }
         
     return (
         <div className="admin">
             <Link className="button-float-right" to="/">&lsaquo;</Link>
             <Link className="button-float-right" to="/admin/add-article">+</Link>
-            <div className="title">Liste des articles</div>
+            <div className="admin-title">Liste des articles</div>
+
+            <CategorySelector onChangeCategory={onCategoryChoice} unselectAvailable={true} />
 
             <ArticlesList articles={articles} />
             
