29b71288d47c714b0c08e78b742edd2e0417a176
celine <atakikoo@gmail.com>
2020-02-06 20:52:06 +0100

    update photos-displayer (display errors )

--- app-tsc/src/shared/photos-displayer/photos-displayer.tsx
+++ app-tsc/src/shared/photos-displayer/photos-displayer.tsx
@@ -2,8 +2,9 @@ import React, {useState, useEffect} from "react";
 import { Photos } from '../../domain/photos-types'
 import firebase from "../../firebase";
 import "firebase/storage";
-import "./photos-displayer";
+import "./photos-displayer.scss"
 import { Carousel } from "../carousel/carousel";
+import {FirebaseStorageError } from '../../components/admin/article-actions'
 
 interface PropsType {
   photos: Photos;
@@ -11,29 +12,39 @@ interface PropsType {
 
 export const PhotosDisplayer = (props: PropsType) => {
   const photos = props.photos;
-  const [previews, setPreviews] = useState<string[]>([]);
+  const [imgUrls, setImgUrls] = useState<string[]>([]);
   const storage = firebase.storage();
+  const [errors, setErrors] = useState<FirebaseStorageError[]>([]);
 
   const doUrlCall = photo => storage.ref().child(`images/${photo}`).getDownloadURL();
   
   const getOneUrlPromise = async photo =>  doUrlCall(photo);
 
-  const getUrls = async () => {
-    const results : Promise<any[]> = Promise.all(props.photos.map(photo => getOneUrlPromise(photo).catch(e => {console.log('ERROR GET URL PHOTO' , e); return e})))
+  const getUrls = async () =>  {
+    const results : Promise<any[]> = Promise.all(props.photos.map(photo => getOneUrlPromise(photo).catch(e => {console.log('ERROR GET URL PHOTO' , e); return e as FirebaseStorageError})))
     const validResults = (await results).filter(result => typeof result === 'string' );
-    return validResults;
+    const errors = (await results).filter(result => typeof result !== 'string' );
+    return {validResults, errors};
   }
 
   useEffect(() => {
-    getUrls().then((urls) => { 
-      setPreviews(urls);
-    })
+    getUrls().then((results) => { 
+      setImgUrls(results.validResults);
+      setErrors(results.errors);
+    }).catch((errs) => console.log('errors catch', errs))
   }, [photos]) 
 
-  if (!previews || previews.length === 0) return null;
+if (errors && errors.length > 0) {
+  return (<div className="firebase-error"> 
+    {errors.map((erro, key) => (
+<div key={key}>{key} ==> {erro.message}</div>
+    ))}
+  </div>) 
+  }
+  if (!imgUrls || imgUrls.length === 0) return null;
   else return (
     <div className="container">
-          <Carousel imgUrls={previews} />
+          <Carousel imgUrls={imgUrls}/>
     </div>
   );
 };
