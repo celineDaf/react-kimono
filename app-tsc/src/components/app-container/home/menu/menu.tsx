55b280d0c5fbc3eab1f33038c482e230f9cbb368
celine <atakikoo@gmail.com>
2020-02-10 12:41:13 +0100

    get articles for home page preview, adjust style

--- app-tsc/src/components/app-container/home/menu/menu.tsx
+++ app-tsc/src/components/app-container/home/menu/menu.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import './menu.scss'
 import SubMenu from './sub-menu/sub-menu'
+import {CategoriesEnum } from '../../../../domain/category-types'
 
 const Menu = () => {
+
+    const categories = CategoriesEnum;
         return (
             <div className="menu">
-                <div className="menu-item right blue-bc">
-                    <SubMenu title="Livres"></SubMenu>
+                <div className="menu-item ">
+                    <SubMenu title="Livres" category={categories.BOOKS} titleSide={'left'} colorBgClass={'blue-bc'}></SubMenu>
                 </div>
-                <div className="menu-item left yellow-bc">
-                    <SubMenu title="Kimono"></SubMenu>
+                <div className="menu-item ">
+                    <SubMenu title="Kimono" category={categories.KIMONO} titleSide={'right'} colorBgClass={'yellow-bc left'}></SubMenu>
                 </div>
-                <div className="menu-item right red-bc">
-                    <SubMenu title="Japon"></SubMenu>
+                <div className="menu-item ">
+                    <SubMenu title="Japon" category={categories.JAPAN} titleSide={'left'} colorBgClass={'red-bc'}></SubMenu>
                 </div>
-                <div className="menu-item left violet-bc">
-                <SubMenu title="Job"></SubMenu>
+                <div className="menu-item ">
+                    <SubMenu title="Job" category={categories.JOB} titleSide={'right'} colorBgClass={'violet-bc  left'}></SubMenu>
                 </div>
             </div>
         );
--- app-tsc/src/components/app-container/home/menu/sub-menu/sub-menu.tsx
+++ app-tsc/src/components/app-container/home/menu/sub-menu/sub-menu.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './sub-menu.scss';
+import ArticlesPreviewSlider from '../articles-preview-slider/articles-preview-slider'
+import { Category } from '../../../../../domain/category-types'
+import firebase from '../../../../../firebase';
+import { IArticle } from '../../../../../domain/article-type'
 
 interface Props {
-    title: string
+    title: string,
+    category: Category,
+    colorBgClass: string,
+    titleSide: 'left' | 'right'
+}
+
+function GetArticlesPreviews(props: Props) {
+
+    const [articles, setArticles]  = useState();
+
+    useEffect(() => { 
+       const unsubscribe = firebase
+           .firestore().collection('articles')
+           .where('category', '==', props.category)
+           .limit(4)
+           .onSnapshot((snapshot)=> {
+               const articles = snapshot.docs.map((doc)=> ({
+                   id: doc.id,
+                   ...doc.data()
+               }));
+               setArticles(articles as IArticle[]);
+           })
+          return () => unsubscribe();
+    }, []); 
+
+    return articles;
 }
 
 const SubMenu = (props: Props) => {
+
+    const articles: IArticle[] = GetArticlesPreviews(props);
+
+    if (!articles) return null;
     return (
         <div className="sub-menu">
-        <div className="title">{props.title}</div>
+            {props.titleSide === 'left' && <span className="title">{props.title}</span>}
+                 <ArticlesPreviewSlider articles={articles} backgroundColorClass={props.colorBgClass}/>
+            {props.titleSide === 'right' && <span className="title">{props.title}</span> }
         </div>
     );
 };
