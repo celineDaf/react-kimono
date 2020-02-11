b86e198e724798982e94a7dc96adae661b97111d
celine <atakikoo@gmail.com>
2020-02-11 18:38:51 +0100

    add horizontal scroll and slide to go to articles list by category

--- app-tsc/src/components/app-container/home/menu/articles-preview-slider/articles-preview-slider.tsx
+++ app-tsc/src/components/app-container/home/menu/articles-preview-slider/articles-preview-slider.tsx
@@ -2,28 +2,44 @@ import React from 'react';
 import { IArticle } from '../../../../../domain/article-type';
 import './articles-preview-slider.scss'
 import ImageDisplay from '../../../../../shared/image-display/image-display'
+import { History, Location } from 'history';
+import { withRouter, match } from 'react-router-dom';
+import { Category } from '../../../../../domain/category-types'
 
 interface Props {
    articles: IArticle[],
-   backgroundColorClass: string
+   backgroundColorClass: string,
+   history: History,
+  location: Location,
+  match: match,
+  category: Category
 }
 
 
 
 const ArticlesPreviewSlider = (props: Props) => {
     
-    
+   const goToArticleList = () => {
+        props.history.push('/articles/category/' + props.category)
+   }
+
+   const goToArticle = (id) => {
+    props.history.push('/articles/' + id)
+}
         return (
-            <div className={'articles-previews-slider ' + props.backgroundColorClass}>
+            <div className={'articles-previews-slider scroll scroll-medium contain-scroll no-gap ' + props.backgroundColorClass}>
                 {props.articles.map((article, key) => (
-                    <div className='slide' key={key}>
+                    <div className='slide flex flex-column' key={key} onClick={() => goToArticle(article.id)}>
                         { article.photos && article.photos[0] ? 
                         <ImageDisplay imageName={article.photos[0]} title={article.title}/> :
-                        <div className="title">{article.title}</div>}
+                        <div className="title-alone">{article.title}</div>}
                     </div>
+                    
                  
                 ))}
-                
+                <div className='slide flex flex-column' onClick={goToArticleList}>
+                    <div className="title all-articles"><div className="icon icon-double-chevron"></div></div>
+                </div>
             </div>
         );
     
@@ -31,4 +47,4 @@ const ArticlesPreviewSlider = (props: Props) => {
     
 };
 
-export default ArticlesPreviewSlider;
+export default withRouter(ArticlesPreviewSlider);
