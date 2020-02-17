bd128edfd79549f2d2fecf8ad8220374f742f637
celine <atakikoo@gmail.com>
2020-02-17 19:52:06 +0100

    add category type for articles list background color

--- app-tsc/src/components/app-container/articles/article-list.tsx
+++ app-tsc/src/components/app-container/articles/article-list.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import { IArticle } from '../../../domain/article-type'
-import ArticleCard from '../../../shared/article-card';
-import './articles.scss'
+import React from "react";
+import { IArticle } from "../../../domain/article-type";
+import ArticleCard from "../../../shared/article-card";
+import "./articles.scss";
+import { Category } from "../../../domain/category-types";
 
 interface PropsType {
-    articles: IArticle[];
-  } 
+  articles: IArticle[];
+  category?: Category;
+}
 const ArticlesList = (props: PropsType) => {
-   
-    return (
-        <div className="articles-list">
-
-            {props.articles && props.articles.length > 0 && props.articles.map((article, index) => {
-                return (
-                    <ArticleCard article={article} key={index} />
-                )
-            })} 
-            
-        </div>
-    );
+  return (
+    <div className={"articles-list " + "articles-list-" + props.category}>
+      {props.articles &&
+        props.articles.length > 0 &&
+        props.articles.map((article, index) => {
+          return <ArticleCard article={article} key={index} />;
+        })}
+    </div>
+  );
 };
 
 export default ArticlesList;
--- app-tsc/src/components/app-container/articles/articles-list-container.tsx
+++ app-tsc/src/components/app-container/articles/articles-list-container.tsx
@@ -1,49 +1,45 @@
-import React, {useEffect} from 'react';
-import ArticlesList from './article-list';
-import { useState } from 'react';
-import firebase from './../../../firebase';
-import './articles.scss';
-import { match } from 'react-router';
+import React, { useEffect } from "react";
+import ArticlesList from "./article-list";
+import { useState } from "react";
+import firebase from "./../../../firebase";
+import "./articles.scss";
+import { match } from "react-router";
+import { Category } from "../../../domain/category-types";
 
 interface Props {
-    match: match
+  match: match;
 }
 
 function useArticles(params) {
+  const [articles, setArticles] = useState();
 
-   
-    const [articles, setArticles]  = useState();
+  useEffect(() => {
+    const unsubscribe = firebase
+      .firestore()
+      .collection("articles")
+      .where("category", "==", params.category)
+      .onSnapshot(snapshot => {
+        const articles = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setArticles(articles);
+      });
+    return () => unsubscribe();
+  }, [params]);
 
-    useEffect(() => { 
-        const unsubscribe = firebase
-            .firestore().collection('articles')
-            .where('category', '==', params.category)
-            .onSnapshot((snapshot)=> {
-                const articles = snapshot.docs.map((doc)=> ({
-                    id: doc.id,
-                    ...doc.data()
-                }));
-                setArticles(articles);
-            })
-            return () => unsubscribe()
-    }, [params]); 
-
-    return articles
+  return articles;
 }
 
 const ArticlesListContainer = (props: Props) => {
-    const articles = useArticles(props.match.params)
-    
-        return (
-            <div className="articles-list-container">
-    
-                <ArticlesList articles={articles} />
-                
-            </div>
-        );
-    
+  const articles = useArticles(props.match.params);
+  const params: { category?: Category } = props.match.params;
 
-    
+  return (
+    <div className="articles-list-container">
+      <ArticlesList articles={articles} category={params.category} />
+    </div>
+  );
 };
 
 export default ArticlesListContainer;
