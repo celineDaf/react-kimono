f912426b51fddf75a3c8ee720bb8755aef74e428
celine <atakikoo@gmail.com>
2020-02-17 19:55:54 +0100

    fix cursor display

--- app-tsc/src/shared/photos-displayer/photos-displayer.tsx
+++ app-tsc/src/shared/photos-displayer/photos-displayer.tsx
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from "react";
-import { Photos } from '../../domain/photos-types'
+import React, { useState, useEffect } from "react";
+import { Photos } from "../../domain/photos-types";
 import firebase from "../../firebase";
 import "firebase/storage";
-import "./photos-displayer.scss"
+import "./photos-displayer.scss";
 import { Carousel } from "../carousel/carousel";
-import {FirebaseStorageError } from '../../components/admin/article-actions'
-import { CarouselDisplay } from '../../domain/carousel-display-type'
+import { FirebaseStorageError } from "../../components/admin/article-actions";
+import { CarouselDisplay } from "../../domain/carousel-display-type";
 
 interface PropsType {
   photos: Photos;
@@ -18,35 +18,55 @@ export const PhotosDisplayer = (props: PropsType) => {
   const storage = firebase.storage();
   const [errors, setErrors] = useState<FirebaseStorageError[]>([]);
 
-  const doUrlCall = photo => storage.ref().child(`images/${photo}`).getDownloadURL();
-  
-  const getOneUrlPromise = async photo =>  doUrlCall(photo);
+  const doUrlCall = photo =>
+    storage
+      .ref()
+      .child(`images/${photo}`)
+      .getDownloadURL();
 
-  const getUrls = async () =>  {
-    const results : Promise<any[]> = Promise.all(props.photos.map(photo => getOneUrlPromise(photo).catch(e => {console.log('ERROR GET URL PHOTO' , e); return e as FirebaseStorageError})))
-    const validResults = (await results).filter(result => typeof result === 'string' );
-    const errors = (await results).filter(result => typeof result !== 'string' );
-    return {validResults, errors};
-  }
+  const getOneUrlPromise = async photo => doUrlCall(photo);
+
+  const getUrls = async () => {
+    const results: Promise<any[]> = Promise.all(
+      props.photos.map(photo =>
+        getOneUrlPromise(photo).catch(e => {
+          console.log("ERROR GET URL PHOTO", e);
+          return e as FirebaseStorageError;
+        })
+      )
+    );
+    const validResults = (await results).filter(
+      result => typeof result === "string"
+    );
+    const errors = (await results).filter(result => typeof result !== "string");
+    return { validResults, errors };
+  };
 
   useEffect(() => {
-    getUrls().then((results) => { 
-      setImgUrls(results.validResults);
-      setErrors(results.errors);
-    }).catch((errs) => console.log('errors catch', errs))
-  }, [photos]) 
+    getUrls()
+      .then(results => {
+        setImgUrls(results.validResults);
+        setErrors(results.errors);
+      })
+      .catch(errs => console.log("errors catch", errs));
+  }, [photos]);
 
-if (errors && errors.length > 0) {
-  return (<div className="firebase-error"> 
-    {errors.map((erro, key) => (
-<div key={key}>{key+1} ==> {erro.message}</div>
-    ))}
-  </div>) 
+  if (errors && errors.length > 0) {
+    return (
+      <div className="firebase-error">
+        {errors.map((erro, key) => (
+          <div key={key}>
+            {key + 1} ==> {erro.message}
+          </div>
+        ))}
+      </div>
+    );
   }
   if (!imgUrls || imgUrls.length === 0) return null;
-  else return (
-    <div className="column container">
-          <Carousel imgUrls={imgUrls} type={props.type}/>
-    </div>
-  );
+  else
+    return (
+      <div className="column">
+        <Carousel imgUrls={imgUrls} type={props.type} />
+      </div>
+    );
 };
