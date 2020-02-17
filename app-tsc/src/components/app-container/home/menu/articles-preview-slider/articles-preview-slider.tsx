4fd02630c2636d94f2526211f0822a8aec162520
celine <atakikoo@gmail.com>
2020-02-17 19:55:06 +0100

    unify slider display with article-cards

--- app-tsc/src/components/app-container/home/menu/articles-preview-slider/articles-preview-slider.tsx
+++ app-tsc/src/components/app-container/home/menu/articles-preview-slider/articles-preview-slider.tsx
@@ -1,50 +1,57 @@
-import React from 'react';
-import { IArticle } from '../../../../../domain/article-type';
-import './articles-preview-slider.scss'
-import ImageDisplay from '../../../../../shared/image-display/image-display'
-import { History, Location } from 'history';
-import { withRouter, match } from 'react-router-dom';
-import { Category } from '../../../../../domain/category-types'
+import React from "react";
+import { IArticle } from "../../../../../domain/article-type";
+import "./articles-preview-slider.scss";
+import ImageDisplay from "../../../../../shared/image-display/image-display";
+import { History, Location } from "history";
+import { withRouter, match } from "react-router-dom";
+import { Category } from "../../../../../domain/category-types";
 
 interface Props {
-   articles: IArticle[],
-   backgroundColorClass: string,
-   history: History,
-  location: Location,
-  match: match,
-  category: Category
+  articles: IArticle[];
+  backgroundColorClass: string;
+  history: History;
+  location: Location;
+  match: match;
+  category: Category;
 }
-
-
 
 const ArticlesPreviewSlider = (props: Props) => {
-    
-   const goToArticleList = () => {
-        props.history.push('/articles/category/' + props.category)
-   }
+  const goToArticleList = () => {
+    props.history.push("/articles/category/" + props.category);
+  };
 
-   const goToArticle = (id) => {
-    props.history.push('/articles/' + id)
-}
-        return (
-            <div className={'articles-previews-slider scroll scroll-medium contain-scroll no-gap ' + props.backgroundColorClass}>
-                {props.articles.map((article, key) => (
-                    <div className='slide flex flex-column' key={key} onClick={() => goToArticle(article.id)}>
-                        { article.photos && article.photos[0] ? 
-                        <ImageDisplay imageName={article.photos[0]} title={article.title}/> :
-                        <div className="title-alone">{article.title}</div>}
-                    </div>
-                    
-                 
-                ))}
-                <div className='slide flex flex-column' onClick={goToArticleList}>
-                    <div className="title all-articles"><div className="icon icon-double-chevron"></div></div>
-                </div>
+  const goToArticle = id => {
+    props.history.push("/articles/" + id);
+  };
+  return (
+    <div
+      className={
+        "articles-previews-slider scroll scroll-medium contain-scroll no-gap " +
+        props.backgroundColorClass
+      }
+    >
+      {props.articles.map((article, key) => (
+        <div
+          className="slide flex flex-column"
+          key={key}
+          onClick={() => goToArticle(article.id)}
+        >
+          {article.photos && article.photos[0] ? (
+            <ImageDisplay imageName={article.photos[0]} title={article.title} />
+          ) : (
+            <div className="title-container">
+              <div className="title-alone">{article.title}</div>
             </div>
-        );
-    
-
-    
+          )}
+        </div>
+      ))}
+      <div className="slide flex flex-column" onClick={goToArticleList}>
+        <div className="icon-all-articles">
+          <div className="icon icon-double-chevron"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(ArticlesPreviewSlider);
