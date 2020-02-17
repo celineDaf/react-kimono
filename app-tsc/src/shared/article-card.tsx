9ebb22bde4412ee7d2b282bd618c9610075d45fd
celine <atakikoo@gmail.com>
2020-02-17 19:53:20 +0100

    remove txt from article-card and place beautifull pictures instead

--- app-tsc/src/shared/article-card.tsx
+++ app-tsc/src/shared/article-card.tsx
@@ -1,34 +1,45 @@
-import React from 'react';
-import { IArticle } from '../domain/article-type'
-import TagsDisplayer from './tags-displayer/tags-displayer';
-import { History, Location } from 'history';
-import { withRouter, match } from 'react-router';
+import React from "react";
+import { IArticle } from "../domain/article-type";
+import TagsDisplayer from "./tags-displayer/tags-displayer";
+import { History, Location } from "history";
+import { withRouter, match } from "react-router";
+import ImageDisplay from "./image-display/image-display";
 
 interface PropsType {
-    article: IArticle,
-    history: History,
-  location: Location,
-  match: match
-  }
+  article: IArticle;
+  history: History;
+  location: Location;
+  match: match;
+}
 
 const ArticleCard = (props: PropsType) => {
-
-    const handleclick = () => {
-        if (/^\/admin/.test(props.location.pathname))  {
-            props.history.push(`/admin/articles/${props.article.id}`);
-        } else {
-            props.history.push(`/articles/${props.article.id}`);
-        }
+  const handleclick = () => {
+    if (/^\/admin/.test(props.location.pathname)) {
+      props.history.push(`/admin/articles/${props.article.id}`);
+    } else {
+      props.history.push(`/articles/${props.article.id}`);
     }
+  };
 
-    return (
-        <div className="article-card" onClick={() => handleclick()}>
-                    <div className="category">{props.article.category}</div>
-                    <div className="title">{props.article.title}</div>
-                    <TagsDisplayer tags={props.article.tags} canRemove={false} />
-                    <div className="preview">{props.article.text}</div>
-        </div>
-    );
+  return (
+    <div className="article-card" onClick={() => handleclick()}>
+      <TagsDisplayer tags={props.article.tags} canRemove={false} />
+      {props.article &&
+        props.article.photos &&
+        props.article.photos.length > 0 && (
+          <ImageDisplay
+            imageName={props.article.photos[0]}
+            title={props.article.title || ""}
+          />
+        )}
+      {props.article &&
+        (!props.article.photos || props.article.photos.length === 0) && (
+          <div className="title-container">
+            <span className="title">{props.article.title}</span>
+          </div>
+        )}
+    </div>
+  );
 };
 
 export default withRouter(ArticleCard);
--- app-tsc/src/shared/image-display/image-display.tsx
+++ app-tsc/src/shared/image-display/image-display.tsx
@@ -1,29 +1,34 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import firebase from "../../firebase";
 import "firebase/storage";
+import "./image-display.scss";
+
 interface Props {
-    imageName: string,
-    title
- }
+  imageName: string;
+  title: string;
+}
 
- const ImageDisplay = (props: Props) => {
-    const [imgUrl, setImgUrl] = useState<string>();
-    const storage = firebase.storage();
+const ImageDisplay = (props: Props) => {
+  const [imgUrl, setImgUrl] = useState<string>();
+  const storage = firebase.storage();
 
-    useEffect(() => {
-        storage.ref().child(`images/${props.imageName}`).getDownloadURL().then((url) => {
-            setImgUrl(url);
-        });
-    }, []);
-    
-    if (!imgUrl) return null;
-    return (
-        <div>
-            <span className="title">{props.title}</span>
-        <img src={imgUrl}/>
-        </div>
-    );
+  useEffect(() => {
+    storage
+      .ref()
+      .child(`images/${props.imageName}`)
+      .getDownloadURL()
+      .then(url => {
+        setImgUrl(url);
+      });
+  }, []);
 
+  if (!imgUrl) return null;
+  return (
+    <div className="image-display">
+      <span className="title">{props.title}</span>
+      <img src={imgUrl} />
+    </div>
+  );
 };
 
 export default ImageDisplay;
