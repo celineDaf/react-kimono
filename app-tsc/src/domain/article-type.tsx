e36000d1bfac441292da3a271bf80c939b1344d5
celine <atakikoo@gmail.com>
2020-02-17 19:26:07 +0100

    fix article type

--- app-tsc/src/domain/article-type.tsx
+++ app-tsc/src/domain/article-type.tsx
@@ -1,12 +1,19 @@
-import { Category, CategoriesEnum } from './category-types'
+import { Category, CategoriesEnum } from "./category-types";
 export interface IArticle {
-    id?: string;
-    tags: string[];
-    title: string | undefined;
-    category: Category | CategoriesEnum.NULL;
-    creationDate: string;
-    text: string | undefined;
-    photos: string[];
+  id?: string;
+  tags: string[];
+  title: string;
+  category: Category | CategoriesEnum.NULL;
+  creationDate: string;
+  text: string | undefined;
+  photos: string[];
 }
 
-export let ArticleInit: IArticle = { tags: [], title: undefined, category: CategoriesEnum.NULL, creationDate: new Date().toDateString(), text: undefined, photos: []};
+export let ArticleInit: IArticle = {
+  tags: [],
+  title: "",
+  category: CategoriesEnum.NULL,
+  creationDate: new Date().toDateString(),
+  text: undefined,
+  photos: []
+};
