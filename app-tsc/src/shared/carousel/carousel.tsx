6f7337567b0b9f588d08a185b2afc4e9a816a3dc
celine <atakikoo@gmail.com>
2020-02-17 20:08:34 +0100

    disable go to next slide when there is only one slide

--- app-tsc/src/shared/carousel/carousel.tsx
+++ app-tsc/src/shared/carousel/carousel.tsx
@@ -1,6 +1,9 @@
-import React,{ useState } from 'react';
+import React, { useState } from "react";
 import "./carousel.scss";
-import { CarouselDisplay, CarouselDisplayEnums } from '../../domain/carousel-display-type'
+import {
+  CarouselDisplay,
+  CarouselDisplayEnums
+} from "../../domain/carousel-display-type";
 
 interface PropsType {
   imgUrls: string[];
@@ -8,30 +11,51 @@ interface PropsType {
 }
 
 export const Carousel = (props: PropsType) => {
-  const slides: string[] =props.imgUrls ;
+  const slides: string[] = props.imgUrls;
   const length = slides.length;
   const [currentSlide, setCurrentSlide] = useState<number>(0);
   const type = props.type || CarouselDisplayEnums.CLASSIC;
   const nextSlide = () => {
-    if (type === CarouselDisplayEnums.PREVIEW) return;
-      const nextSlide = currentSlide + 1 === length -1 ? 0 : currentSlide +1;
-      setCurrentSlide(nextSlide);
-  }
+    if (type === CarouselDisplayEnums.PREVIEW || length === 1) return;
+    const nextSlide = currentSlide + 1 === length - 1 ? 0 : currentSlide + 1;
+    setCurrentSlide(nextSlide);
+  };
 
   return (
-    <div className={'carousel ' + type } >
-     <div className='slide-container'>
+    <div className={"carousel " + type}>
+      <div className="slide-container">
         {slides.map((slide, key) => (
-           <div className="slide"
-              style={{'opacity': key === currentSlide || type === CarouselDisplayEnums.PREVIEW ? 1 : 0,
-              'cursor': key === currentSlide || type === CarouselDisplayEnums.PREVIEW? 'pointer' : 'none',
-              'zIndex': key === currentSlide || type === CarouselDisplayEnums.PREVIEW? 10 : 0}} 
-               key={key} onClick={nextSlide}>
-              <img  src={slide}  alt={'Slide ' + key}/>
-             {type === CarouselDisplayEnums.CLASSIC && <span className='counter' >{key+1} / {length}</span>}
+          <div
+            className="slide"
+            style={{
+              opacity:
+                key === currentSlide || type === CarouselDisplayEnums.PREVIEW
+                  ? 1
+                  : 0,
+              cursor:
+                key === currentSlide &&
+                (type === CarouselDisplayEnums.PREVIEW || length > 1)
+                  ? "pointer"
+                  : "none",
+              zIndex:
+                key === currentSlide || type === CarouselDisplayEnums.PREVIEW
+                  ? 10
+                  : 0
+            }}
+            key={key}
+            onClick={nextSlide}
+          >
+            <img src={slide} alt={"Slide " + key} />
+            {type === CarouselDisplayEnums.CLASSIC &&
+              slides &&
+              slides.length > 1 && (
+                <span className="counter">
+                  {key + 1} / {length}
+                </span>
+              )}
           </div>
-          ))}
-          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 };
