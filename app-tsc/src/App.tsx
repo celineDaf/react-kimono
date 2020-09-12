7c793d65d8452334ba77ff1480f90f6bc90b49ce
Céline Daffe <atakikoo@gmail.com>
2020-09-12 18:13:01 +0200

    remove admin routing

--- app-tsc/src/App.tsx
+++ app-tsc/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch} from "react-router-dom";
-import Admin from './components/admin/admin';
-import AdminArticleUpdateContainer from './components/admin/admin-article/admin-article-update-container';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AppContainer from './components/app-container/app-container';
 
 
@@ -10,14 +8,11 @@ const App: React.FC = () => {
   return (
     <div className="app">
       <BrowserRouter>
-          <Switch>
-            <Route path="/admin" exact component={Admin}/>
-            <Route path="/admin/add-article" exact component={AdminArticleUpdateContainer}/>
-            <Route path="/admin/articles/:id" exact component={AdminArticleUpdateContainer}/>
-            <Route path="/" component={AppContainer}/>
-          </Switch>
+        <Switch>
+          <Route path="/" component={AppContainer} />
+        </Switch>
       </BrowserRouter>
-      </div>
+    </div>
   );
 }
 
