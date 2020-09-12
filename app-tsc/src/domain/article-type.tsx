10e49cbef560bccef48445857d8237440e1924a1
Céline Daffe <atakikoo@gmail.com>
2020-09-12 18:15:45 +0200

    update article type with prismic

--- app-tsc/src/domain/article-type.tsx
+++ app-tsc/src/domain/article-type.tsx
@@ -1,19 +1,73 @@
 import { Category, CategoriesEnum } from "./category-types";
+
 export interface IArticle {
-  id?: string;
-  tags: string[];
-  title: string;
-  category: Category | CategoriesEnum.NULL;
-  creationDate: string;
-  text: string | undefined;
-  photos: string[];
+  id: string,
+  uid: string,
+  type: string,
+  href: string,
+  tags: string[],
+  first_publication_date: string,
+  last_publication_date: string,
+  slugs: string[],
+  linked_documents: [],
+  lang: string,
+  alternate_languages: [],
+  data: {
+    title: [
+      {
+        type: string,
+        text: string,
+        spans: []
+      }
+    ],
+    content: any[],
+    contentVideos: PrismicVideo[],
+    contentImages: PrismicImage[]
+    thumbnail: PrismicImage
+  }
 }
 
-export let ArticleInit: IArticle = {
-  tags: [],
-  title: "",
-  category: CategoriesEnum.NULL,
-  creationDate: new Date().toDateString(),
-  text: undefined,
-  photos: []
+
+
+
+
+
+export type PrismicImage = {
+  dimensions: {
+    width: number,
+    height: number
+  },
+  url: string,
+  alt: string,
+  copyright: string,
 };
+
+export type PrismicVideo = {
+  width: number,
+  provider_name: string,
+  html: string,
+  thumbnail_width: number,
+  title: string,
+  version: string,
+  author_name: string,
+  height: number,
+  type: string,
+  provider_url: string,
+  thumbnail_height: 360,
+  thumbnail_url: string,
+  embed_url: string
+}
+
+export type PrismicTitle = [
+  {
+    type: string,
+    text: string,
+    spans: []
+  }
+]
+
+export type PrismicLink = {
+  name: string,
+  title: string
+}
+
