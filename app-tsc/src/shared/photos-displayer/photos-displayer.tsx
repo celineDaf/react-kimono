eb2aad2e6c3bcd8d95709e681d6f90d3b9f2f23f
Céline Daffe <atakikoo@gmail.com>
2020-09-13 12:26:09 +0200

    update photos-displayer

--- app-tsc/src/shared/photos-displayer/photos-displayer.tsx
+++ app-tsc/src/shared/photos-displayer/photos-displayer.tsx
@@ -1,67 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { Photos } from "../../domain/photos-types";
-import firebase from "../../firebase";
-import "firebase/storage";
 import "./photos-displayer.scss";
 import { Carousel } from "../carousel/carousel";
-import { FirebaseStorageError } from "../../helpers/firebase-helpers";
 import { CarouselDisplay } from "../../domain/carousel-display-type";
+import { IPrismicImage } from '../../domain/priscmic-types';
+
 
 interface PropsType {
-  photos: Photos;
+  photos: IPrismicImage[];
   type?: CarouselDisplay;
 }
 
 export const PhotosDisplayer = (props: PropsType) => {
-  const photos = props.photos;
   const [imgUrls, setImgUrls] = useState<string[]>([]);
-  const storage = firebase.storage();
-  const [errors, setErrors] = useState<FirebaseStorageError[]>([]);
-
-  const doUrlCall = photo =>
-    storage
-      .ref()
-      .child(`images/${photo}`)
-      .getDownloadURL();
-
-  const getOneUrlPromise = async photo => doUrlCall(photo);
-
-  const getUrls = async () => {
-    const results: Promise<any[]> = Promise.all(
-      props.photos.map(photo =>
-        getOneUrlPromise(photo).catch(e => {
-          console.log("ERROR GET URL PHOTO", e);
-          return e as FirebaseStorageError;
-        })
-      )
-    );
-    const validResults = (await results).filter(
-      result => typeof result === "string"
-    );
-    const errors = (await results).filter(result => typeof result !== "string");
-    return { validResults, errors };
-  };
 
   useEffect(() => {
-    getUrls()
-      .then(results => {
-        setImgUrls(results.validResults);
-        setErrors(results.errors);
-      })
-      .catch(errs => console.log("errors catch", errs));
-  }, [photos]);
+    const urls = props.photos.map((p) => p.url);
+    setImgUrls(urls);
+  }, [props.photos])
 
-  if (errors && errors.length > 0) {
-    return (
-      <div className="firebase-error">
-        {errors.map((erro, key) => (
-          <div key={key}>
-            {key + 1} ==> {erro.message}
-          </div>
-        ))}
-      </div>
-    );
-  }
   if (!imgUrls || imgUrls.length === 0) return null;
   else return <Carousel imgUrls={imgUrls} type={props.type} />;
 };
