8873033b0ee3537c6a6bbc1b4d5882cfc0b6d6e0
Céline Daffe <atakikoo@gmail.com>
2020-09-13 12:30:33 +0200

    update article service

--- app-tsc/src/services/article-service.tsx
+++ app-tsc/src/services/article-service.tsx
@@ -1,100 +1,83 @@
-import { IArticle } from "../domain/article-type";
+// import { IArticle } from "../domain/article-type";
 import { Category } from "../domain/category-types";
-import firebase from "./../firebase";
-import {
-  FirebaseResponseType,
-  FirebaseResponseInit
-} from "../helpers/firebase-helpers";
-
-const getOptions = {
-  source: "cache"
-};
+import PrismicService from '../prismic-configuration';
+import { IArticle } from '../domain/article-type';
+import { Document } from 'prismic-javascript/types/documents';
+import ApiSearchResponse from 'prismic-javascript/types/ApiSearchResponse';
 
 class Service {
-  
-  getArticlesById(id: string): Promise<FirebaseResponseType<IArticle>> {
-    let response = new FirebaseResponseInit() as FirebaseResponseType<IArticle>;
-    const unsubscribe = firebase
-      .firestore()
-      .collection("articles")
-      .doc(id)
-      .get()
-      .then(doc => {
-        if (doc.exists) {
-          response.content = { id: doc.id, ...doc.data() } as IArticle;
-        } else {
-          console.log("error article does not exists ", id);
-          response.messageError = "error article " + id + " does not exists ";
-        }
-        return response;
-      })
-      .catch(err => {
-        console.log("error fetch article by id", id, err);
-        response.messageError = "error article " + id + " does not exists ";
-        return response;
-      });
 
-    return unsubscribe;
+  getArticleById(id: string): Promise<Document> {
+    // Promise<ResponseType<IArticle>> 
+
+    // const [article, setArticle] = useState();
+    // const f = async () => {
+    //  let response: Promise<ResponseType<IArticle>> = async new ResponseInit();
+
+    // if
+    //   LocalStorageService.checkLocalStorage(category) &&
+    //   LocalStorageService.shouldUpdateCategoryArticlesList(category)
+    // ) {
+    //  response = await LocalStorageService.getArticleByIdFromLocalstorage(
+    //   category,
+    //   id
+    //  );
+    // if (response.messageError) console.log(response.messageError);
+    // } else {
+    // const response: any = await 
+    return PrismicService.getArticleById(id);
+
+    // setArticle(response.results);
+    // }
+    // }
+    // f();
+
+    // return article;
   }
 
-  getArticlesByCategoryLimit5(
-    category: Category
-  ): Promise<FirebaseResponseType<IArticle[]>> {
-    let response = new FirebaseResponseInit() as FirebaseResponseType<
-      IArticle[]
-    >;
-    const unsubscribe = firebase
-      .firestore()
-      .collection("articles")
-      .where("category", "==", category)
-      .limit(5)
-      .get()
-      .then(querySnapshot => {
-        response.content = querySnapshot.docs.map(
-          doc =>
-            ({
-              id: doc.id,
-              ...doc.data()
-            } as IArticle)
-        );
-        return response;
-      })
-      .catch(err => {
-        response.messageError = "error fetch articles by category " + category;
-        console.log("error fetch articles by category", category, err);
-        return response;
-      });
-    return unsubscribe;
+  getArticlesByCategoryLimit5(category: Category): Promise<ApiSearchResponse> {
+    // const [articles, setArticles] = useState();
+    // const f = async () => {
+    //     // if (
+    //     //   LocalStorageService.checkLocalStorage(category) &&
+    //     //   LocalStorageService.shouldUpdateCategoryArticlesList(category)
+    //     // ) {
+    //     const response = await LocalStorageService.getArticlesListFromLocalStorage(
+    //       category,
+    //       5
+    //     );
+    //     setArticles(response);
+    //     // } else {
+    // const response: any = await 
+    return PrismicService.getArticlesByCategories(
+      category, 5
+    );
+    // setArticles(response.results);
+    // console.log('getArticlesByCategoryLimit5', response.results)
   }
+  // };
+  // f();
 
-  getArticlesByCategory(
-    category: Category
-  ): Promise<FirebaseResponseType<IArticle[]>> {
-    let response = new FirebaseResponseInit() as FirebaseResponseType<
-      IArticle[]
-    >;
-    const unsubscribe = firebase
-      .firestore()
-      .collection("articles")
-      .where("category", "==", category)
-      .get()
-      .then(querySnapshot => {
-        response.content = querySnapshot.docs.map(
-          doc =>
-            ({
-              id: doc.id,
-              ...doc.data()
-            } as IArticle)
-        );
-        return response;
-      })
-      .catch(err => {
-        response.messageError = "error fetch articles by category " + category;
-        console.log("error fetch articles by category", category, err);
-        return response;
-      });
-    return unsubscribe;
-  }
+  // return articles;
+  // }
+
+  // getArticlesByCategory(category: Category): any {
+
+  // }
+
+  // getArticlesByCategory(
+  //   category: Category
+  // ): Promise < ResponseType < IArticle[] >> {}
+
+  // getNextArticlesInCategory(
+  //   category: Category,
+  //   limit?: number
+  // ): Promise<ResponseType<IArticle[]>> { }
+
+  // getPreviousArticlesInCategory(
+  //   category: Category,
+  //   limit?: number
+  // ): Promise<ResponseType<IArticle[]>> { }
 }
 
 const ArticleService = new Service();
