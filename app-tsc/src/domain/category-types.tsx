bb668ce5e9747d50f4e0d995cf25735139f4f2ab
Céline Daffe <atakikoo@gmail.com>
2020-09-28 12:27:46 +0200

    update category types, remove null

--- app-tsc/src/domain/category-types.tsx
+++ app-tsc/src/domain/category-types.tsx
@@ -1,9 +1,9 @@
 
 export enum CategoriesEnum {
-    JAPAN = 'japon', JOB = 'job', KIMONO = 'kimono', BOOKS = 'books', NULL = 'null'
+    JAPAN = 'japon', JOB = 'job', KIMONO = 'kimono', BOOKS = 'books'
 };
 
-export type Category = CategoriesEnum.BOOKS | CategoriesEnum.JAPAN | CategoriesEnum.JOB | CategoriesEnum.KIMONO | CategoriesEnum.NULL;
+export type Category = CategoriesEnum.BOOKS | CategoriesEnum.JAPAN | CategoriesEnum.JOB | CategoriesEnum.KIMONO;
 
 export const CategoriesList: Category[] = [
     CategoriesEnum.BOOKS,
