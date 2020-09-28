178f528905ea31e1a0c3c5974244714518f228c8
Céline Daffe <atakikoo@gmail.com>
2020-09-28 12:27:19 +0200

    update carousel

--- app-tsc/src/shared/carousel/carousel.tsx
+++ app-tsc/src/shared/carousel/carousel.tsx
@@ -4,20 +4,22 @@ import {
   CarouselDisplay,
   CarouselDisplayEnums
 } from "../../domain/carousel-display-type";
+import { ISlideContent } from '../photos-displayer/photos-displayer';
+import { RichText } from 'prismic-reactjs';
 
 interface PropsType {
-  imgUrls: string[];
+  content: ISlideContent[];
   type?: CarouselDisplay;
 }
 
 export const Carousel = (props: PropsType) => {
-  const slides: string[] = props.imgUrls;
+  const slides: ISlideContent[] = props.content;
   const length = slides.length;
   const [currentSlide, setCurrentSlide] = useState<number>(0);
   const type = props.type || CarouselDisplayEnums.CLASSIC;
   const nextSlide = () => {
     if (type === CarouselDisplayEnums.PREVIEW || length === 1) return;
-    const nextSlide = currentSlide + 1 === length - 1 ? 0 : currentSlide + 1;
+    const nextSlide = currentSlide + 1 === length ? 0 : currentSlide + 1;
     setCurrentSlide(nextSlide);
   };
 
@@ -34,7 +36,7 @@ export const Carousel = (props: PropsType) => {
                   : 0,
               cursor:
                 key === currentSlide &&
-                (type === CarouselDisplayEnums.PREVIEW || length > 1)
+                  (type === CarouselDisplayEnums.PREVIEW || length > 1)
                   ? "pointer"
                   : "none",
               zIndex:
@@ -45,13 +47,14 @@ export const Carousel = (props: PropsType) => {
             key={key}
             onClick={nextSlide}
           >
-            <img src={slide} alt={"Slide " + key} />
+            <img src={slide.url} alt={"Slide " + key} />
             {type === CarouselDisplayEnums.CLASSIC &&
               slides &&
               slides.length > 1 && (
-                <span className="counter">
-                  {key + 1} / {length}
-                </span>
+                <div className="info">
+                  <div>{RichText.render(slide.text)}</div>
+                  <div>{key + 1} / {length}</div>
+                </div>
               )}
           </div>
         ))}
