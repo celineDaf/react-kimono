0c411d9aa608c5a3e2cc74f8cdaa869f5190a7bc
Céline Daffe <atakikoo@gmail.com>
2020-09-29 23:05:30 +0200

    add icons rights

--- app-tsc/src/components/app-container/footer/footer.tsx
+++ app-tsc/src/components/app-container/footer/footer.tsx
@@ -3,16 +3,18 @@ import './footer.scss'
 
 const Footer = () => {
     return (
-            <div className="footer">
-                <div className="content">
+        <div className="footer">
+            <div className="content">
                 <div>Icons made by <a href="https://www.flaticon.com/authors/chanut" title="Chanut">Chanut</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
                 <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
                 <div>Icons made by <a href="https://www.flaticon.com/authors/mynamepong" title="mynamepong">mynamepong</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
                 <div>Icons made by <a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26">Icongeek26</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
                 <div>Icons made by <a href="https://www.flaticon.com/<?=_('authors/')?>good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-                </div>
+                <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></div>
+                <div>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></div>
             </div>
-            );
+        </div>
+    );
 }
 
 export default Footer
